973b42f4c80133bca2f4bf5d1f20cd32a115d807
rodelgabriel45@gmail.com <rodelgabriel45@gmail.com>
2024-04-26 10:31:44 +0800

    Add persist redux for storing current user

--- client/store/store.js
+++ client/store/store.js
@@ -1,9 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
 
 import userReducer from "./user/userSlice.js";
 
-const store = configureStore({
-  reducer: { user: userReducer },
+const rootReducer = combineReducers({
+  user: userReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export default store;
