46e487707d5dbf4efabe3993a99f8f2ec15fc04b
rodelgabriel45@gmail.com <rodelgabriel45@gmail.com>
2024-05-22 22:57:16 +0800

    Complete application

--- api/index.js
+++ api/index.js
@@ -27,9 +27,17 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
+
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
